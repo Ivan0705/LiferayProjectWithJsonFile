036f6ee3787be3d971766c42c2037b966fa25ce7
Ivan <vipanda569@mail.ru>
2022-05-31 16:28:58 +0700

    Simple test

--- modules/PostPortletFromJson/src/main/java/test/SimpleTest.java
+++ modules/PostPortletFromJson/src/main/java/test/SimpleTest.java
@@ -1,12 +1,33 @@
 package test;
 
+import com.google.gson.Gson;
 import gsonParcer.GsonParserPost;
 import model.Items;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimpleTest {
     public static void main(String[] args) {
         GsonParserPost gsonParserPost = new GsonParserPost();
         Items items = gsonParserPost.parse();
-        System.out.println(items);
+        String str1 = items.toString();
+        System.out.println(str1);
+
+        Gson gson = new Gson();
+        String str2 = gson.toJson(items);
+        System.out.println(str2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void fromJson() throws IOException {
+        String strItems1 = new String(Files.readAllBytes(Paths.get("C:\\Users\\Ivan\\IdeaProjects\\LiferayProjectWithJsonFile\\modules\\PostPortletFromJson\\src\\main\\resources\\post.json")), StandardCharsets.UTF_8);
+        String strItems2 = new String(Files.readAllBytes(Paths.get("C:\\Users\\Ivan\\IdeaProjects\\LiferayProjectWithJsonFile\\modules\\PostPortletFromJson\\src\\main\\resources\\post.json")), StandardCharsets.UTF_8);
+
+        assertEquals(strItems1, strItems2);
     }
 }
